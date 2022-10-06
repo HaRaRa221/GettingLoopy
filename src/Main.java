4c981e6b6a6fd96e32109682c03e789c3e053446
Raymond Ha <Haro@mail.uc.edu>
2022-10-06 15:28:26 -0400

    Added code for add/subtract number and stars

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args)
+    {
+        for(int x = 0; x <= 30; x++)
+        {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+
+        for (int n = 30; n >= 0; n--)
+        {
+            System.out.print(n + " ");
+        }
+
+        for (int n = 0; n <= 18; n = n + 3)
+        {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+
+        for (int n = 10; n >= 0; n = n - 2)
+        {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+
+        for (int r = 1; r <= 5; r++)
+        {
+            for (int c = 1; c <= 5; c++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int r = 1; r <= 5; r++)
+        {
+            for (int c = 1; c <= r; c++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int r = 5; r >= 1; r--)
+        {
+            for (int c = 1; c <= r; c++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+     }
 }
--- src/forLoop4.java
+++ src/forLoop4.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class forLoop4 {
+public class forLoop4
+{
+    public static void main(String[] args)
+    {
+        for (int n = 10; n >= 0; n = n - 2)
+        {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+    }
 }
